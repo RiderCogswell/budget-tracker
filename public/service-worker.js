4a2436163e4925f3c226f78ac0b28f3ff0abf570
Ridercogswell <ridercogswell@gmail.com>
2022-05-10 02:17:17 -0700

    downloadable pwa

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,18 +4,19 @@ const VERSION = 'version01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 // must be relative paths
 const FILES_TO_CACHE = [
-    './public/index.html',
-    './public/css/styles.css',
-    './public/js/index.js',
-    './public/js/idb.js',
-    './public/icons/icon-72x72.png',
-    './public/icons/icon-96x96.png',
-    './public/icons/icon-128x128.png',
-    './public/icons/icon-144x144.png',
-    './public/icons/icon-152x152.png',
-    './public/icons/icon-192x192.png',
-    './public/icons/icon-384x384.png',
-    './public/icons/icon-512x512.png'
+    './index.html',
+    './css/styles.css',
+    './js/index.js',
+    './js/idb.js',
+    './manifest.json',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png'
 ];
 
 // cant use window element because service workers run before window is created
